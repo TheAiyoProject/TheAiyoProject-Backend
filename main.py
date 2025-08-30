5a318eaeba2acd3a68ad8fadd85783f35b8743a5
CPTCIC5 <aryanjainak@gmail.com>
2025-08-30 23:28:09 +0530

    XYZ

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-#app.add_middleware(HTTPSRedirectMiddleware)
+app.add_middleware(HTTPSRedirectMiddleware)
 
 
 static_path = Path("static")
