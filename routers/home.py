f7c1e7108b7e50bc858bef145ed6e9d65ae95d08
CPTCIC5 <aryanjainak@gmail.com>
2025-09-23 18:47:36 +0530

    XYZ

--- routers/home.py
+++ routers/home.py
@@ -15,6 +15,8 @@ mongo_client = MongoClient(mongo_url)
 
 @router.get("/dashboard")
 async def user_data_dashboard(current_user: User = Depends(get_current_user)):
+    if not current_user.is_verified:
+        return "Verify your account to access the dashboard"
     my_platform_id= current_user.platform_id
     print(my_platform_id)
     return "XYZ"
