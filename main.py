fdc2b0e4cbf5ee90f4a5024ad30d9dbc7e9547d9
CPTCIC5 <aryanjainak@gmail.com>
2025-09-26 12:02:20 +0530

    XYZ

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app.add_middleware(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://aiyo-frontend.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
